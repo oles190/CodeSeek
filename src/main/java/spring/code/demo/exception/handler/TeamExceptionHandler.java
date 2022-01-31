2019000f00b08d868adad0ecf9f64de5b0c2b32e
Oles <doskucho@gmail.com>
2022-01-31 15:44:58 +0200

    handler

--- src/main/java/spring/code/demo/exception/handler/TeamExceptionHandler.java
+++ src/main/java/spring/code/demo/exception/handler/TeamExceptionHandler.java
@@ -3,6 +3,7 @@ package spring.code.demo.exception.handler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import spring.code.demo.exception.team.*;
 
@@ -10,34 +11,46 @@ import spring.code.demo.exception.team.*;
 @ControllerAdvice
 public class TeamExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(value = TeamNotFoundException.class)
-    protected ResponseEntity handle(TeamNotFoundException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+
+
+    @ExceptionHandler(TeamNotFoundException.class)
+    public ResponseEntity<ErrorMessage> handleException(TeamNotFoundException e) {
+        ErrorMessage errorMessage = new ErrorMessage(e.getMessage());
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(value = TeamBalanceException.class)
-    protected ResponseEntity handle(TeamBalanceException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(TeamBalanceException.class)
+    public ResponseEntity<ErrorMessage> handleException(TeamBalanceException e) {
+        ErrorMessage errorMessage = new ErrorMessage(e.getMessage());
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(TeamCityException.class)
+    public ResponseEntity<ErrorMessage> handleException(TeamCityException e) {
+        ErrorMessage errorMessage = new ErrorMessage(e.getMessage());
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(value = TeamCityException.class)
-    protected ResponseEntity handle(TeamCityException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(TeamCommissionException.class)
+    public ResponseEntity<ErrorMessage> handleException(TeamCommissionException e) {
+        ErrorMessage errorMessage = new ErrorMessage(e.getMessage());
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(value = TeamCommissionException.class)
-    protected ResponseEntity handle(TeamCommissionException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(TeamCountryException.class)
+    public ResponseEntity<ErrorMessage> handleException(TeamCountryException e) {
+        ErrorMessage errorMessage = new ErrorMessage(e.getMessage());
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(value = TeamCountryException.class)
-    protected ResponseEntity handle(TeamCountryException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(TeamNameException.class)
+    public ResponseEntity<ErrorMessage> handleException(TeamNameException e) {
+        ErrorMessage errorMessage = new ErrorMessage(e.getMessage());
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(value = TeamNameException.class)
-    protected ResponseEntity handle(TeamNameException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
-    }
+
+
+
+
 
 }
